58fdc5625e1d256d1a2e8ecd93635f1f6149f737
Jan-David van Binsbergen <jan-david.van.binsbergen@ulb.ac.be>
2016-11-07 10:48:16 +0100

    Added IntensityShuttleThunderbolt support to local_ffmpeg_hls

--- tmpl_sources/div_main_header.php
+++ tmpl_sources/div_main_header.php
@@ -2,7 +2,7 @@
 <div class="header">
     <div class="logo">
         <a href="http://www.ulb.ac.be"><img src="./images/Generale/logo-ulb.png"/></a>
-       <a href="./"><img src="images/Generale/logoEZ.png"></a>
+        <a href="./"><img src="images/Generale/logoEZ.png"></a>
     </div>
 </div>
 
