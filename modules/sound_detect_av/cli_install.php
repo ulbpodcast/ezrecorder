83fbad64146f1507ed2ce8e6c3667b88025edd36
Jan-David <jvbinsbe@ulb.ac.be>
2017-02-08 16:15:37 +0100

    Fixed install script for sound_detect_av

--- modules/sound_detect_av/cli_install.php
+++ modules/sound_detect_av/cli_install.php
@@ -1,5 +1,11 @@
 <?php
 
+$conf_file = __DIR__."/config.inc";
+if(file_exists($conf_file) && filesize($conf_file) > 0)
+    require_once $conf_file;
+else
+    require_once __DIR__.'/config_sample.inc';
+
 require_once(__DIR__."/../../lib_install.php");
 
 echo PHP_EOL . "******************************************" . PHP_EOL;
@@ -8,13 +14,24 @@ echo           "******************************************" . PHP_EOL;
 
 $config = file_get_contents(__DIR__ ."/config_sample.inc");
 
-
 $value = read_line("Use given avinterface to detect sound level [default: '$vu_meter_avfoundation_index']: ");
 if ($value != "")
     $vu_meter_avfoundation_index = $value;
 //else keep default
+
+$value = read_line("Use given avinterface to detect sound level [default: '$vu_meter_avfoundation_remote_username']: ");
+if ($value != "")
+    $vu_meter_avfoundation_remote_username = $value;
+//else keep default
+
+$value = read_line("Use given avinterface to detect sound level [default: '$vu_meter_avfoundation_remote_ip']: ");
+if ($value != "")
+    $vu_meter_avfoundation_remote_ip = $value;
+//else keep default
     
 $config = preg_replace('/\$vu_meter_avfoundation_index = (.+);/', '\$vu_meter_avfoundation_index = "' . $vu_meter_avfoundation_index . '";', $config);
+$config = preg_replace('/\$vu_meter_avfoundation_remote_username = (.+);/', '\$vu_meter_avfoundation_remote_username = "' . $vu_meter_avfoundation_remote_username . '";', $config);
+$config = preg_replace('/\$vu_meter_avfoundation_remote_ip = (.+);/', '\$vu_meter_avfoundation_remote_ip = "' . $vu_meter_avfoundation_remote_ip . '";', $config);
 
 echo PHP_EOL . "Creating 'config.inc'...";
 file_put_contents(__DIR__."/config.inc", $config);
