05c7b7622429c4f46dbd3b425f939e461544820f
Arnaud Willame <arwillame@mac-1S0-276.local>
2019-08-02 14:07:48 +0200

    try fix

--- modules/local_ffmpeg_hls/bash/ffmpeg_stop.php
+++ modules/local_ffmpeg_hls/bash/ffmpeg_stop.php
@@ -6,7 +6,7 @@ Stops the recording and start post processing
 
 require_once __DIR__."/../etc/config.inc";
 require_once __DIR__."/../lib_capture.php";
-require_once "$basedir/lib_various.php";    
+require_once "$basedir/lib_various.php";
 require_once __DIR__."/../info.php";
 require_once "$basedir/lib_ffmpeg.php";
 
@@ -26,17 +26,17 @@ $video_file_name = $argv[2];
 if(file_exists($ffmpeg_monitoring_file))
     unlink($ffmpeg_monitoring_file);
 
-#stop recording 
+#stop recording
 stop_ffmpeg($ffmpeg_pid_file);
 stop_ffmpeg($ffmpeg_pid2_file); //low stream if any
-      
+
 $cmd = "$php_cli_cmd $ffmpeg_script_process $asset $video_file_name";
 echo $cmd;
 
 $return_val = 0;
 system($cmd, $return_val);
 if($return_val != 0) {
-    $logger->log(EventType::RECORDER_FFMPEG_STOP, LogLevel::WARNING, 
+    $logger->log(EventType::RECORDER_FFMPEG_STOP, LogLevel::WARNING,
             "Call to process script failed, error code: $return_val.", array($log_context));
     exit(3);
 }
@@ -46,18 +46,18 @@ exit(0);
 // ######################################################
 // ######################################################
 // ######################################################
-
+/*
 function stop_ffmpeg($pid_file) {
     global $logger;
     global $log_context;
-    
+
     if(file_exists($pid_file)) {
         $pid = file_get_contents($pid_file);
         unlink($pid_file);
         $return_val = 0;
         system("kill -2 $pid", $return_val);
         if($return_val != 0) {
-            $logger->log(EventType::RECORDER_FFMPEG_STOP, LogLevel::ERROR, 
+            $logger->log(EventType::RECORDER_FFMPEG_STOP, LogLevel::ERROR,
                     "Could not kill FFMPEG process (pid $pid)", array($log_context));
             return false;
         }
@@ -76,3 +76,4 @@ function stop_ffmpeg($pid_file) {
     }
     return true;
 }
+*/
