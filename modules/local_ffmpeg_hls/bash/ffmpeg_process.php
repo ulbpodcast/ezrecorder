37b9c69ce3e81fe523584e2deefc9b5a2e0b005a
Arnaud Willame <arwillame@mac-1S0-276.local>
2019-08-02 13:59:16 +0200

    Try to fix

--- modules/local_ffmpeg_hls/bash/ffmpeg_process.php
+++ modules/local_ffmpeg_hls/bash/ffmpeg_process.php
@@ -6,7 +6,7 @@ Stops the recording and start post processing
 
 require_once __DIR__."/../etc/config.inc";
 require_once __DIR__."/../lib_capture.php";
-require_once "$basedir/lib_various.php";    
+require_once "$basedir/lib_various.php";
 require_once __DIR__."/../info.php";
 require_once "$basedir/lib_ffmpeg.php";
 
@@ -26,10 +26,10 @@ $process_dir = get_asset_module_folder($module_name, $asset);
 $asset_dir = get_asset_dir($asset);
 $pid_file = "$process_dir/process_pid";
 $cutlist_file = ffmpeg_get_cutlist_file($module_name, $asset);
-$process_result_file = "$process_dir/$process_result_filename";
+$process_result_file = "$process_dir/ffmpeg/$process_result_filename";
 
 if(!file_exists($asset_dir)) {
-    $logger->log(EventType::RECORDER_FFMPEG_PROCESS, LogLevel::CRITICAL, 
+    $logger->log(EventType::RECORDER_FFMPEG_PROCESS, LogLevel::CRITICAL,
             "Could not find asset dir $asset_dir", array($log_context));
     exit(2);
 }
@@ -42,30 +42,30 @@ echo $cmd;
 $return_val = 0;
 system($cmd, $return_val);
 if($return_val != 0) {
-    $logger->log(EventType::RECORDER_FFMPEG_PROCESS, LogLevel::WARNING, 
+    $logger->log(EventType::RECORDER_FFMPEG_PROCESS, LogLevel::WARNING,
             "Call to merge movies failed, error code: $return_val. Check merge_movies.log", array($log_context));
-    file_put_contents($process_result_file, $return_val); 
+    file_put_contents($process_result_file, $return_val);
 }
 
 //if merge movies return 2, merge has been successfully but cutting failed. Let's continue with what we got.
 if($return_val == 5) {
-    $logger->log(EventType::RECORDER_FFMPEG_PROCESS, LogLevel::ERROR, 
+    $logger->log(EventType::RECORDER_FFMPEG_PROCESS, LogLevel::ERROR,
             "Parts merge succeeded but cutting failed. This is BAD, but let's continue with what we got.", array($log_context));
     //at this point, merge movie script should still have produced a cam.mov file
 } else if($return_val != 0) {
     exit(1);
 }
 
-$logger->log(EventType::RECORDER_FFMPEG_PROCESS, LogLevel::DEBUG, 
+$logger->log(EventType::RECORDER_FFMPEG_PROCESS, LogLevel::DEBUG,
         "ffmpeg_process finished movie merging", array($log_context));
 
 //move resulting cam file to asset folder (from ffmpeg folder)
 rename("$process_dir/$video_file_name", "$asset_dir/$video_file_name");
 
 #all okay, write success
-file_put_contents($process_result_file, "0"); 
+file_put_contents($process_result_file, "0");
 
-$logger->log(EventType::RECORDER_FFMPEG_PROCESS, LogLevel::INFO, 
+$logger->log(EventType::RECORDER_FFMPEG_PROCESS, LogLevel::INFO,
         "ffmpeg_process finished successfully", array($log_context));
 
 exit(0);
