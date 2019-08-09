d2fc3c97387bf18249ffa9a2207f07830d3d006b
root <root@Debian-9-amd64.qcow2>
2019-08-09 10:54:41 +0200

    local_ffmpeg_hls: add perms to all subdirectory of etc & var

--- modules/local_ffmpeg_hls/setperms_samplinux.sh
+++ modules/local_ffmpeg_hls/setperms_samplinux.sh
@@ -7,7 +7,7 @@ chown -R !USER:!WEB_USER ./var
 #chmod +a "!USER allow list,add_file,search,add_subdirectory,delete_child,file_inherit,directory_inherit" ./var
 #chmod +a "!WEB_USER allow list,add_file,search,add_subdirectory,delete_child,file_inherit,directory_inherit" ./var
 #chgrp -R !WEB_USER ./var
-chmod ug=rwx ./var
+chmod -R ug=rwx ./var
 touch ./var/status
 chown -R !USER:!WEB_USER ./var/status
 #chmod +a "!USER allow read,write,execute,append" ./var/status
@@ -19,4 +19,4 @@ chown -R !USER:!WEB_USER ./remote/var
 #chmod +a "!WEB_USER allow list,add_file,search,add_subdirectory,delete_child,file_inherit,directory_inherit" ./etc
 chown !USER ./etc
 chgrp !WEB_USER ./etc
-chmod ug=rwx ./etc
+chmod -R ug=rwx ./etc
